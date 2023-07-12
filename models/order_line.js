1b6ff55288f25a783be20974ec797e2e5be5790e
hatem attia <h.attia@teamdev.tn3>
2023-07-12 11:39:46 +0100

    order line

--- models/order_line.js
+++ models/order_line.js
@@ -1,49 +1,42 @@
-
-
-
-
 module.exports = (sequelize, DataTypes) => {
-    const OrderLine = sequelize.define('OrderLine', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
+    const OrderLine = sequelize.define(
+        'OrderLine',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+
+            status: { type: DataTypes.STRING, allowNull: false },
+            total: { type: DataTypes.INTEGER, allowNull: false },
+            total_discount: { type: DataTypes.INTEGER, allowNull: false },
+            quantity: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+            discount: { type: DataTypes.INTEGER, allowNull: false }
         },
-      
-        
-        status: { type: DataTypes.STRING, allowNull: false , defaultValue: "Pending"},
-        total: { type: DataTypes.INTEGER, allowNull: false },
-        total_discount: { type: DataTypes.INTEGER, allowNull: false },
-        quantity: { type: DataTypes.INTEGER, allowNull: false , defaultValue: 0},
-        discount: { type: DataTypes.INTEGER, allowNull: false },
-        
-        
-         
-          
+        {
+            sequelize,
+            paranoid: true
 
-
-    },{
-        sequelize,
-        paranoid: true,
-      
-        // If you want to give a custom name to the deletedAt column
-        // deletedAt: 'destroyTime'
-      });
-    OrderLine.associate = models => {
+            // If you want to give a custom name to the deletedAt column
+            // deletedAt: 'destroyTime'
+        }
+    );
+    OrderLine.associate = (models) => {
         OrderLine.belongsTo(models.Order, {
             foreignKey: {
                 allowNull: false
             },
-            onDelete: "cascade"
+            onDelete: 'cascade'
         });
         OrderLine.belongsTo(models.Product, {
             foreignKey: {
                 allowNull: false
             },
-            onDelete: "cascade"
+            onDelete: 'cascade'
         });
-    }
+    };
     // OrderLine.associate = models => {
     //     OrderLine.belongsTo(models.Product, {
     //         foreignKey: {
@@ -54,13 +47,3 @@ module.exports = (sequelize, DataTypes) => {
     // }
     return OrderLine;
 };
-
-
-
-
-
-
-
-
-
-
