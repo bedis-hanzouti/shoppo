55c8414029e87131794b9ef45da9108cbc38fae3
hatem attia <h.attia@teamdev.tn3>
2023-07-12 11:01:47 +0100

    delete mongose

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const { Sequelize } = require('sequelize')
-const fs = require('fs')
-const db = require('./models')
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const db = require('./models');
 var path = require('path');
 const figlet = require('figlet');
 // db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-const logger = require('morgan')
-const cors = require("cors");
-require("dotenv/config");
-const authJwt = require("./helpers/jwt");
-const errorHandler = require("./helpers/error-handler");
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
 app.use(cors());
-app.options("*", cors());
-const stream = fs.createWriteStream(path.join(__dirname, 'logger.log'), { flags: "a" })
-app.use(logger('combined', { stream: process.env.NODE_ENV === "production" ? stream : "" }));
+app.options('*', cors());
+const stream = fs.createWriteStream(path.join(__dirname, 'logger.log'), { flags: 'a' });
+app.use(logger('combined', { stream: process.env.NODE_ENV === 'production' ? stream : '' }));
 
 //middleware
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 // app.use(authJwt());
-app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 // app.use(errorHandler);
 
 //Routes
-const categoriesRoutes = require("./routes/categories");
-const productsRoutes = require("./routes/products");
-const usersRoutes = require("./routes/users");
-const customerssRoutes = require("./routes/customer");
-const imagesRoutes = require("./routes/image");
-const ordersRoutes = require("./routes/orders");
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const customerssRoutes = require('./routes/customer');
+const imagesRoutes = require('./routes/image');
+const ordersRoutes = require('./routes/orders');
 
 const api = process.env.API_URL;
 
@@ -46,25 +45,22 @@ app.use(`${api}/customer`, customerssRoutes);
 app.use(`${api}/image`, imagesRoutes);
 app.use(`${api}/orders`, ordersRoutes);
 
+db.sequelize
+    .sync({ alter: true, force: false })
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('server running in port 3000');
 
-
-
-db.sequelize.sync({ alter: true, force: false }).then(() => {
-  app.listen(3000, () => {
-    console.log("server running in port 3000")
-
-    figlet("H3B - ENGINE", function (err, data) {
-      if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-      }
-      console.log(data);
+            figlet('H3B - ENGINE', function (err, data) {
+                if (err) {
+                    console.log('Something went wrong...');
+                    console.dir(err);
+                    return;
+                }
+                console.log(data);
+            });
+        });
+    })
+    .catch((error) => {
+        console.error('Unable to connect to the database: ', error.message);
     });
-  })
-}).catch((error) => {
-  console.error('Unable to connect to the database: ', error.message);
-});
-
-
-
