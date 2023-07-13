4bafe05c4a2c395668248f87eb7cc7b77b38d44c
hatemAttia <39554815+hatemAttia@users.noreply.github.com>
2023-07-13 09:12:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(`${api}/image`, imagesRoutes);
 app.use(`${api}/orders`, ordersRoutes);
 
 db.sequelize
-    .sync({ alter: false, force: true })
+    .sync({ alter: false, force: false })
     .then(() => {
         app.listen(3000, () => {
             console.log('server running in port 3000');
