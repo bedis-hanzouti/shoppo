7f5be94e6455baf625884988099694fab5f828a6
Badis Hanzouti <85877823+bedis-hanzouti@users.noreply.github.com>
2023-07-16 16:45:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(`${api}/image`, imagesRoutes);
 app.use(`${api}/orders`, ordersRoutes);
 
 db.sequelize
-    .sync({ alter: true, force: true })
+    .sync({ alter: false, force: false })
     .then(() => {
         app.listen(3000, () => {
             console.log('server running in port 3000');
